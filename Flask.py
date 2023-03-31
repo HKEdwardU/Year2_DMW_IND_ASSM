11eb3ea99da3580f71729234b8adb70a09bbc87c
HKEdwardU <93186062+HKEdwardU@users.noreply.github.com>
2023-03-31 18:31:39 +0800

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect, session
 from tabulate import tabulate
 import params
 import pymysql
-import time
 
 app = Flask(__name__)
 app.secret_key = 'ABCDE'
